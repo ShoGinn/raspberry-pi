4c010b835d5699793d2320328f35bf4b9bee5757
Scott <2660603+ShoGinn@users.noreply.github.com>
2022-09-24 13:48:20 -0400

    fix: cleaned up the scripts

--- scripts/raspi-config.sh
+++ scripts/raspi-config.sh
@@ -1,41 +1,155 @@
 #!/usr/bin/env bash
+# This file:
 #
-CONFIG=/boot/config.txt
-spinner_pid=
+#  - This is a bash script to setup the default paramaters for a headless rpi
+#
+# Usage:
+#
+#  see help
+#
+# Based on a template by BASH3 Boilerplate v2.3.0
+# http://bash3boilerplate.sh/#authors
+#
+# The MIT License (MIT)
+# Copyright (c) 2013 Kevin van Zonneveld and contributors
+# You are not obligated to bundle the LICENSE file with your b3bp projects as long
+# as you leave these references intact in the header comments of your source files.
+
+### BASH3 Boilerplate (b3bp) Header
+##############################################################################
+
+# Commandline options. This defines the usage page, and is used to parse cli
+# opts & defaults from. The parsing is unforgiving so be precise in your syntax
+# - A short option must be preset for every long option; but every short option
+#   need not have a long option
+# - `--` is respected as the separator between options and arguments
+# - We do not bash-expand defaults, so setting '~/app' as a default will not resolve to ${HOME}.
+#   you can use bash variables to work around this (so use ${HOME} instead)
+
+# shellcheck disable=SC2034
+read -r -d '' __usage <<-'EOF' || true # exits non-zero when EOF encountered
+  -r --run         Run the script.
+  -v               Enable verbose mode, print script as it is executed
+  -d --debug       Enables debug mode
+  -h --help        This page
+  -n --no-color    Disable color output
+EOF
+
+# shellcheck disable=SC2034
+read -r -d '' __helptext <<-'EOF' || true # exits non-zero when EOF encountered
+ Do a headless setup for the rpi!
+EOF
+
+# shellcheck source=main.sh
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/main.sh"
+
+### Signal trapping and backtracing
+##############################################################################
+
+function __b3bp_cleanup_before_exit() {
+    info "Cleaning up. Done"
+}
+trap __b3bp_cleanup_before_exit EXIT
+
+# requires `set -o errtrace`
+__b3bp_err_report() {
+    local error_code=${?}
+    # shellcheck disable=SC2154
+    error "Error in ${__file} in function ${1} on line ${2}"
+    exit ${error_code}
+}
+# Uncomment the following line for always providing an error backtrace
+# trap '__b3bp_err_report "${FUNCNAME:-.}" ${LINENO}' ERR
+
+### Command-line argument switches (like -d for debugmode, -h for showing helppage)
+##############################################################################
+
+# debug mode
+if [[ "${arg_d:?}" = "1" ]]; then
+    set -o xtrace
+    PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+    LOG_LEVEL="7"
+    # Enable error backtracing
+    trap '__b3bp_err_report "${FUNCNAME:-.}" ${LINENO}' ERR
+fi
+
+# verbose mode
+if [[ "${arg_v:?}" = "1" ]]; then
+    set -o verbose
+fi
+
+# no color mode
+if [[ "${arg_n:?}" = "1" ]]; then
+    NO_COLOR="true"
+fi
+
+# help mode
+if [[ "${arg_h:?}" = "1" ]]; then
+    # Help exists with code 1
+    help "Help using ${0}"
+fi
+
+### Validation. Error out if the things required for your script are not present
+##############################################################################
+
+[[ "${arg_r:?}" == "1" ]] || help "You must use -r as it is required."
+[[ "${LOG_LEVEL:-}" ]] || emergency "Cannot continue without LOG_LEVEL. "
+
+#Check if the script is ran by root
+__system_user_name=$(id -un)
+if [[ "${__system_user_name}" != 'root' ]]; then
+    error 'This script must be run as root.'
+    exit 1
+fi
+
+### Runtime
+##############################################################################
+
+__boot_config=/boot/config.txt
 
 disable_ipv6() {
-    # Disable IPv6
-    echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee /etc/sysctl.d/disable-ipv6.conf >/dev/null
-    echo "blacklist ipv6" | sudo tee /etc/modprobe.d/blacklist-ipv6.conf >/dev/null
+    info "Disabling ipv6."
+    echo "net.ipv6.conf.all.disable_ipv6 = 1" | tee /etc/sysctl.d/disable-ipv6.conf >/dev/null
+    echo "blacklist ipv6" | tee /etc/modprobe.d/blacklist-ipv6.conf >/dev/null
 }
 
 disable_audio() {
-    sed -i '/dtparam=audio/c dtparam=audio=off' $CONFIG
+    info "Disabling Audio."
+    sed -i '/dtparam=audio/c dtparam=audio=off' $__boot_config
 }
 disable_bluetooth() {
+    info "Disabling Bluetooth."
     dtoverlay_entry disable-bt
     stop_and_mask bluetooth
     # Remove bluetooth dependencies
-    sudo apt-get purge bluez piwiz -y -qq >/dev/null
-    sudo apt-get autoremove --purge -y -qq >/dev/null
-
+    apt-get purge bluez piwiz -y -qq >/dev/null
+    apt-get autoremove --purge -y -qq >/dev/null
 }
+
 disable_wifi() {
+    info "Disabling WiFi."
     dtoverlay_entry disable-wifi
     stop_and_mask wpa_supplicant hciuart
-
 }
+
 dtoverlay_entry() {
-    if ! grep -q "dtoverlay=$1" $CONFIG; then
-        echo "dtoverlay=$1" | sudo tee -a $CONFIG >/dev/null
+    if ! grep -q "dtoverlay=$1" $__boot_config; then
+        echo "dtoverlay=$1" | tee -a $__boot_config >/dev/null
     fi
-
 }
+
 stop_and_mask() {
-    sudo systemctl stop "$@"
-    sudo systemctl mask "$@"
+    systemctl stop "$@"
+    systemctl mask "$@"
+}
+update_apt() {
+    info "Updating the apt packages."
+    apt-get update >/dev/null
+    apt-get -y upgrade -qq >/dev/null
+
 }
 execute_raspi_config() {
+    info "Updating the raspi-config settings."
     grep -E -v -e '^\s*#' -e '^\s*$' <<END |
 \
 # Hardware Configuration
@@ -54,47 +168,19 @@ do_change_locale en_US.UTF-8
 
 # Don't add any raspi-config configuration options after 'END' line below & don't remove 'END' line
 END
-        sed -e 's/$//' -e 's/^\s*/\/usr\/bin\/raspi-config nonint /' | sudo bash -
+        sed -e 's/$//' -e 's/^\s*/\/usr\/bin\/raspi-config nonint /' | bash -
 
 }
 
-function start_spinner {
-    set +m
-    echo -n "$1         "
-    { while :; do for X in '  •     ' '   •    ' '    •   ' '     •  ' '      • ' '     •  ' '    •   ' '   •    ' '  •     ' ' •      '; do
-        echo -en "\b\b\b\b\b\b\b\b$X"
-        sleep 0.1
-    done; done & } 2>/dev/null
-    spinner_pid=$!
-}
-
-function stop_spinner {
-    { kill -9 $spinner_pid && wait; } 2>/dev/null
-    set -m
-    echo -en "\033[2K\r"
-}
-
-trap stop_spinner EXIT
-
-lsb_release -ds
-start_spinner "Updating apt. This could take a while ..."
-(
-    sudo apt-get update >/dev/null
-    wait
-    sudo apt-get -y upgrade -qq >/dev/null
-)
-stop_spinner
-wait
+update_apt
 # Disable Stuff
-start_spinner "Disabling Bluetooth, WiFi, Audio and ipv6 and setting Locales"
 disable_audio
 disable_ipv6
 disable_bluetooth
 disable_wifi
 execute_raspi_config
 stop_and_mask ModemManager.service
-stop_spinner
 
-start_spinner "Rebooting in 5 seconds"
+warning "Rebooting in 5 seconds"
 sleep 5
-sudo reboot
+reboot
